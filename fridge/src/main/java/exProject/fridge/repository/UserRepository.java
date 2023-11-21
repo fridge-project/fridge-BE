80de066cfd96d132408f7d71ab09e748ce9c417b
JSG <jsgcys0815@gmail.com>
2023-11-22 03:19:39 +0900

    Update UserRepository.java

--- fridge/src/main/java/exProject/fridge/repository/UserRepository.java
+++ fridge/src/main/java/exProject/fridge/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    User findByUsernameAndPassword(String username, String password);
+    User findByEmailAndPassword(String email, String password);
 
-    User findByUsername(String username);
+    User findByEmail(String email);
 }
