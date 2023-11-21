e180baeb902d5001c774676c6bea005f23aaad8f
JSG <jsgcys0815@gmail.com>
2023-11-22 03:20:30 +0900

    Update FridgeApiController.java

--- fridge/src/main/java/exProject/fridge/apiController/FridgeApiController.java
+++ fridge/src/main/java/exProject/fridge/apiController/FridgeApiController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// 주석 테스트
 @RestController
 public class FridgeApiController {
 
