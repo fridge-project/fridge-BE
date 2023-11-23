2a340a6df759dee0fa1fc6755da5dab2aa0dd785
JSG <jsgcys0815@gmail.com>
2023-11-24 04:01:00 +0900

    Update FridgeApiController.java

--- fridge/src/main/java/exProject/fridge/apiController/FridgeApiController.java
+++ fridge/src/main/java/exProject/fridge/apiController/FridgeApiController.java
@@ -2,8 +2,16 @@ package exProject.fridge.apiController;
 
 import exProject.fridge.dto.ResponseDto;
 import exProject.fridge.model.Fridge;
+import exProject.fridge.model.Ingredient;
+import exProject.fridge.model.StorageType;
+import exProject.fridge.model.User;
+import exProject.fridge.repository.IngredientRepository;
 import exProject.fridge.service.FridgeService;
+import exProject.fridge.service.IngredientService;
 import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,21 +25,46 @@ public class FridgeApiController {
     private FridgeService fridgeService;
 
     @Autowired
+    private IngredientService ingredientService;
+
+    @Autowired
     private HttpSession session;
 
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    static class RequestIngre {
+        private String name;
+        private String exp;
+        private String memo;
+        private String storage;
+    }
 
     @PostMapping("/fridge") // 재료 등록
-    public ResponseDto<Integer> addIngredients(@RequestBody Fridge fridge) {
+    public ResponseDto<Integer> addIngredients(@RequestBody RequestIngre requestIngre) {
+        Fridge fridge = new Fridge();
+        fridge.setExp(requestIngre.getExp());
+        fridge.setMemo(requestIngre.getMemo());
+        fridge.setStorage(StorageType.valueOf(requestIngre.getStorage()));
 
+        Ingredient ingredient = ingredientService.getIngredient(requestIngre.getName());
+        fridge.setIngredient(ingredient);
+
+        User user = (User)(session.getAttribute("principal"));
         // 1. 재료가 있나?
-        boolean exist;
+//        boolean exist = fridgeService.isExist(user.getId(), ingredient.getId());
+
         // 2-1. 동일 재료가 이미 있으면 실패 return
+//        if(exist) return new ResponseDto<>(HttpStatus.UNAUTHORIZED.value(), 0);
+
         // 2-2. 동일 재료가 없으면 등록 후 성공 return
-        try {
-            addIngredients(fridge);
-        } catch (Exception e) {
-            return new ResponseDto<>(HttpStatus.UNAUTHORIZED.value(), 0);
-        }
+        fridge.setUser(user);
+
+        System.out.println("-----------");
+        System.out.println(fridge);
+        System.out.println("-----------");
+
+        fridgeService.addIngredient(fridge);
 
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
