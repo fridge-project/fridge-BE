2b9ba959fecb26c103b4c10edc33e7fdaf262e6e
JSG <jsgcys0815@gmail.com>
2023-11-23 17:16:57 +0900

    Update FridgeApiController.java

--- fridge/src/main/java/exProject/fridge/apiController/FridgeApiController.java
+++ fridge/src/main/java/exProject/fridge/apiController/FridgeApiController.java
@@ -1,25 +1,46 @@
 package exProject.fridge.apiController;
 
 import exProject.fridge.dto.ResponseDto;
+import exProject.fridge.model.Fridge;
+import exProject.fridge.service.FridgeService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FridgeApiController {
+    @Autowired
+    private FridgeService fridgeService;
+
+    @Autowired
+    private HttpSession session;
+
+
+    @PostMapping("/fridge") // 재료 등록
+    public ResponseDto<Integer> addIngredients(@RequestBody Fridge fridge) {
+
+        // 1. 재료가 있나?
+        boolean exist;
+        // 2-1. 동일 재료가 이미 있으면 실패 return
+        // 2-2. 동일 재료가 없으면 등록 후 성공 return
+        try {
+            addIngredients(fridge);
+        } catch (Exception e) {
+            return new ResponseDto<>(HttpStatus.UNAUTHORIZED.value(), 0);
+        }
+
+        return new ResponseDto<>(HttpStatus.OK.value(), 1);
+    }
 
     @GetMapping("/fridge") // 보유 재료 확인
     public ResponseDto<String> getIngredients() {
 
 
         return new ResponseDto<>(HttpStatus.OK.value(), "미완");
-    }
-
-    @PostMapping("/fridge") // 재료 등록
-    public ResponseDto<Integer> addIngredients() {
-
-        return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 
 }
