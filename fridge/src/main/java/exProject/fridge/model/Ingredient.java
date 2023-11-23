73b399d7ef1de8c0a7ec676d5ad92f03c28bd18e
JSG <jsgcys0815@gmail.com>
2023-11-23 13:19:06 +0900

    Update Ingredient.java

--- fridge/src/main/java/exProject/fridge/model/Ingredient.java
+++ fridge/src/main/java/exProject/fridge/model/Ingredient.java
@@ -14,11 +14,11 @@ import lombok.NoArgsConstructor;
 public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int id; // PK_auto
 
     @Column(nullable = false, length = 20, unique = true)
-    private String name;
+    private String name; // 재료명
 
     @Column(nullable = false, length = 100)
-    private String imageURL;
+    private String imageURL; // 이미지url
 }
