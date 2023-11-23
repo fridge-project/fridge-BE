b1249c382e47d4ba42e09fd149a0cabdea9a7f33
JSG <jsgcys0815@gmail.com>
2023-11-23 13:52:45 +0900

    Update Fridge.java

--- fridge/src/main/java/exProject/fridge/model/Fridge.java
+++ fridge/src/main/java/exProject/fridge/model/Fridge.java
@@ -12,29 +12,25 @@ import lombok.NoArgsConstructor;
 @Builder
 @Entity
 public class Fridge {
-    // id(AutoIncrement)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 외래키 두개 합쳐서 PK로 변경
     private int id;
 
-    // 사용자 id - 외래키
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
-    User user;
+    User user; // 사용자 id - 외래키
 
-    // 재료코드 - 외래키
     @ManyToOne
     @JoinColumn(name = "ingre_id", referencedColumnName = "id")
-    Ingredient ingredient;
+    Ingredient ingredient; // 재료코드 - 외래키
 
-    // 유통기한 - null 가능
     @Column(length = 30) // 날짜로 변경해야함
-    private String exp;
+    private String exp; // 유통기한 - null 가능
 
     @Enumerated(EnumType.STRING)
-    private StorageType storage;
+    private StorageType storage; // 보관 방법
 
     @Column(length = 100)
-    private String memo;
+    private String memo; // 메모
 
 }
