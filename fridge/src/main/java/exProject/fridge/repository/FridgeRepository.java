8138c57e8e7797ac39a4ca6602481ef1a6256140
JSG <jsgcys0815@gmail.com>
2023-11-24 19:45:46 +0900

    Update FridgeRepository.java

--- fridge/src/main/java/exProject/fridge/repository/FridgeRepository.java
+++ fridge/src/main/java/exProject/fridge/repository/FridgeRepository.java
@@ -12,5 +12,7 @@ public interface FridgeRepository extends JpaRepository<Fridge, FridgeId> {
     @Query(value = "SELECT f FROM Fridge f WHERE f.user_id = :user_id AND f.ingre_id = :ingre_id", nativeQuery = true)
     Fridge findIngredients(int user_id, int ingre_id);
 
+    Fridge findByUserId(int user_id);
+
 //    Fridge findByingreId(int ingre_id);
 }
